b490669302613d6cdb7a4108f502adad3ebd4312
Kerem Ayseli <46223333+KeremAyseli@users.noreply.github.com>
2020-12-09 06:57:10 +0300

    Sınıflar Yazıldı ve Diyagramlar eklendi.
    Gerekli tablo sınıflarını ve bunları kullanmak için gerekli veritabanı bağlantı kodlarını yazdım.Gerekli uml ve nesne şemasını çıkarttım.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,13 @@
 package com.company;
 
+import Sınıflar.Arabalar;
 import Sınıflar.VeriTabaniIslemleri;
 public class Main {
 
     public static void main(String[] args) {
         VeriTabaniIslemleri veritabanı=new VeriTabaniIslemleri();
         veritabanı.Baglan();
+       Arabalar araba= veritabanı.VerileriGetirAraba(1);
+
     }
 }
